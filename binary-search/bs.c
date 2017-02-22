3aa60b657b549ac998374869e9fef5d57481b403
Neil Grogan <neil@grogan.ie>
2017-02-22 23:25:19 +0000

    Crude first ver of bs

--- binary-search/bs.c
+++ binary-search/bs.c
@@ -1,4 +1,30 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+void makeGuess(int target){
+  printf("Enter a guess:\n");
+  int guess;
+  scanf("%d", &guess);
+  checkGuess(target, guess);
+}
+
+int checkGuess(int target, int guess){
+  if(guess > target){
+    printf("You guessed too high!\n");
+    makeGuess(target);
+  } else if(guess < target){
+    printf("You guessed too low!\n");
+    makeGuess(target);
+  } else {
+    printf("You are correct!\n");
+    exit(0);
+  }
+}
 
 int main(int code, char** args){
-	return 0;
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int target = arr[rand() % 10];
+  makeGuess(target);
+  return 0;
 }
+
