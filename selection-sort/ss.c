a270fe5f6992939a1e82e6d0ea38d2c5799d235f
Neil Grogan <neil@grogan.ie>
2017-02-28 21:25:36 +0000

    Started selection sort

---
+++ selection-sort/ss.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+void sort(int arrToSort[], int sortedArr[]){
+}
+
+
+int main(int code, char** args){
+  int arrToSort[10] = {11, 12, 2, 4, 55, 36, 35, 88, 29, 10};
+  int sortedArr = malloc(sizeof(int)*10)
+  sort(arrToSort, sortedArr);
+  return 0;
+}
+
