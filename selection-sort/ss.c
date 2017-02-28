9ed4134f136871d2858c4986f1e5e794f9871eea
Neil Grogan <neil@grogan.ie>
2017-02-28 21:36:24 +0000

    Printing array on selection sort

--- selection-sort/ss.c
+++ selection-sort/ss.c
@@ -1,15 +1,19 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <time.h>
 
-void sort(int arrToSort[], int sortedArr[]){
+void sort(int * arrToSort, int * sortedArr){
+  for(int i=0; i < sizeof(arrToSort); i++){
+    printf("%d\n",arrToSort[i]);
+  }
 }
+
+
 
 
 int main(int code, char** args){
   int arrToSort[10] = {11, 12, 2, 4, 55, 36, 35, 88, 29, 10};
-  int sortedArr = malloc(sizeof(int)*10)
-  sort(arrToSort, sortedArr);
+  int *sortedArr = malloc(sizeof(int)*10);
+  sort((int *) arrToSort, (int *) sortedArr);
   return 0;
 }
 
